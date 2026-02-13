fbdb2e70e73821cf37644067010648817b6d68c0
agus-darmawan <wayanagus.dr@gmail.com>
2026-02-13 11:44:35 +0700

    fix: biome config with tailwind v4 and remve shadcn

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,24 @@
-import { useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-import { use } from "react";
+"use client";
+// import { useTranslations } from "next-intl";
+// import { setRequestLocale } from "next-intl/server";
+// import { use } from "react";
 
 export default function IndexPage({
 	params,
 }: {
 	params: Promise<{ locale: string }>;
 }) {
-	const { locale } = use(params);
+	// const { locale } = use(params);
 
-	setRequestLocale(locale);
+	// setRequestLocale(locale);
 
 	// Once the request locale is set, you
 	// can call hooks from `next-intl`
-	const t = useTranslations("HomePage");
+	// const t = useTranslations("HomePage");
 
 	return (
-		<main>
-			<h1>{t("title")}</h1>
+		<main className="w-full h-screen bg-ubuntu-dark overflow-hidden select-none">
+			<div className="bg-ubuntu-dark text-black p-6">Ubuntu Style Card</div>
 		</main>
 	);
 }
